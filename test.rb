a01a231fa19b3ecd942628a391da4f0b600f2ac1
mansamikail <fifthharamone05@gmail.com>
2022-05-31 10:38:20 +0600

    eq

--- test.rb
+++ test.rb
@@ -1,1 +1,8 @@
-puts(10+20)
+puts 'enter the first number'
+firstnamber = gets.chomp
+puts 'enter the second number'
+secondnumber = gets.chomp
+puts "multiplied: #{firstnamber.to_i * secondnumber.to_i}"
+puts "division: #{firstnamber.to_i / secondnumber.to_i}"
+puts "addition: #{firstnamber.to_i + secondnumber.to_i}"
+puts "subtraction: #{firstnamber.to_i - secondnumber.to_i}"
